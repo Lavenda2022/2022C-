e0e95ed71d44a514ecaeb29ef4041b2dc316727a
Lavenda <103087649+Lavenda2022@users.noreply.github.com>
2022-11-21 20:11:54 +1300

    Update and rename 6.4.cpp to insert_array.cpp

--- insert_array.cpp
+++ insert_array.cpp
@@ -1,6 +1,7 @@
 // Write a program that reads in an array of 6 real numbers,
 // deletes the value at index 2 and displays the changed contents of the array,
 // then inserts 6.75 at index 1 and displays the contents again.
+
 #include<iostream>
 using namespace std;
 
