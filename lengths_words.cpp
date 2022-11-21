7e164ea6f5c30f56cbdca63abbe10491cec5b138
Lavenda <103087649+Lavenda2022@users.noreply.github.com>
2022-11-21 20:27:38 +1300

    Update and rename 9.1.cpp to lengths_words.cpp

--- lengths_words.cpp
+++ lengths_words.cpp
@@ -1,5 +1,6 @@
 // Write a program that asks the user for three words, then display the lengths of the three words.
 // Display the sum of these three lengths and the length of the three words concatenated (which should be identical).
+
 #include<iostream>
 #include<string>
 using namespace std;
